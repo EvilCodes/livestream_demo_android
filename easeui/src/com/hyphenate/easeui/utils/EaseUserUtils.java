21d14cc1b1b5d97fbc47bd89acd8b7c0206844c1
耿小泰 <clawpo@foxmail.com>
2017-04-17 10:33:52 +0800

    主播暂停界面的头像和昵称显示

--- easeui/src/com/hyphenate/easeui/utils/EaseUserUtils.java
+++ easeui/src/com/hyphenate/easeui/utils/EaseUserUtils.java
@@ -6,6 +6,7 @@ import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.hyphenate.chat.EMClient;
 import com.hyphenate.easeui.R;
 import com.hyphenate.easeui.controller.EaseUI;
 import com.hyphenate.easeui.controller.EaseUI.EaseUserProfileProvider;
@@ -131,5 +132,13 @@ public class EaseUserUtils {
                 textView.setText(user.getMUserName());
             }
         }
+    }
+
+    public static void setCurrentNick(TextView textView) {
+        setAppUserNick(EMClient.getInstance().getCurrentUser(),textView);
+    }
+
+    public static void setCurrentAvatar(Context context, ImageView imageView) {
+        setAppUserAvatar(context,EMClient.getInstance().getCurrentUser(),imageView);
     }
 }
